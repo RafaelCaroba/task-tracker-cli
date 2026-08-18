1b03243a5bb70691cca88634b6e2855c8ae66ee6
Rafael Caroba de Souza <rafa.caroba.17@gmail.com>
2026-08-18 18:51:11 -0300

    Setup inicial do projeto.

---
+++ src/main/java/br/com/caroba/tasktracker/Main.java
@@ -0,0 +1,14 @@
+package br.com.caroba.tasktracker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Task Tracker inicializado!");
+
+        String nome = "Caroba";
+
+        System.out.println("Olá, " + nome + "!");
+    }
+    
+}
+
